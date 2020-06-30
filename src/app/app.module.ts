f6862d945cd9682d1b7afeb12da7aea0ce1c1d02
tusharbalar <balar.tushar1@gmail.com>
2020-06-30 07:43:52 +0530

    Added header and footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { SearchComponent } from './components/search/search.component';
 import { SearchResultComponent } from './components/search-result/search-result.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
-    SearchResultComponent
+    SearchResultComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
